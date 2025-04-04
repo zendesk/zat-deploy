69068311087eecaed8e23cc8578be8bc4b141226
Ashwin Shenoy <ashwinkshenoy@gmail.com>
2025-04-04 13:54:52 +0530

    add path structure for upload (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,30 @@
 #!/bin/sh -l
 
-echo "Deploying to Zendesk..."
-FILE=.zat
+APP_PATH="${1:-.}"
 
+echo "Deploying to Zendesk..."
+echo "Switching to directory: $APP_PATH"
+
+cd "$APP_PATH" || {
+  echo "Error: Failed to change directory to $APP_PATH"
+  exit 1
+}
+
+ls -la
+
+FILE=.zat
 PASSWORD="\"password\""
 APP_ID="\"app_id\""
 
-if  grep -q "$APP_ID" "$FILE" ; then
-    echo "App ID detected, running update" ; 
+if grep -q "$APP_ID" "$FILE" ; then
+    echo "App ID detected, running update"
     zat clean
     zat update
 elif grep -q "$PASSWORD" "$FILE" ; then
-    echo "No existing APP ID detected, creating new app." ; 
+    echo "No existing APP ID detected, creating new app."
     zat clean
     zat create
 else
-    echo "Can't deploy. Please check your .zat file" ; 
+    echo "Can't deploy. Please check your .zat file"
+    exit 1
 fi
